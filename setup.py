4c5ed1c990b12caca71e9b9a04db28f08a874d5f
Joe Greener <jgreener@hotmail.co.uk>
2023-03-29 17:10:03 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="progres",
-    version="0.1.3",
+    version="0.2.0",
     author="Joe G Greener",
     author_email="jgreener@mrc-lmb.cam.ac.uk",
     description="Fast protein structure searching using structure graph embeddings",
